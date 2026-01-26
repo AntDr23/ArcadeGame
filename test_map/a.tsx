eda8696285d8dc5d0f28fd0da734859a506c92cb
AntDr23 <dronanton2010new@gmail.com>
2026-01-26 18:47:52 +0300

    Add files via upload

--- test_map/a.tsx
+++ test_map/a.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="a" tilewidth="70" tileheight="70" spacing="2" tilecount="64" columns="8">
- <image source="pythonProject/kenney_platformer-art-deluxe/Base pack/Items/items_spritesheet.png" width="576" height="576"/>
+ <image source="../pythonProject/kenney_platformer-art-deluxe/Base pack/Items/items_spritesheet.png" width="576" height="576"/>
 </tileset>
