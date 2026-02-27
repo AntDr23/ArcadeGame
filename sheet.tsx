03dbdf48025c40647cadb3769b97c55052c38acf
AntDr23 <dronanton2010new@gmail.com>
2026-02-27 21:38:24 +0300

    Add files via upload

---
+++ sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="sheet" tilewidth="70" tileheight="70" spacing="2" tilecount="78" columns="13">
+ <image source="../kenney_platformer-art-deluxe/Ice expansion/sheet.png" width="980" height="490"/>
+</tileset>
