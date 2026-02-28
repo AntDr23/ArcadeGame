35913d24346d6ca3dae10710e1271519f20ed13f
lydishka <129724887+lydishka@users.noreply.github.com>
2026-02-28 19:58:53 +0300

    Delete ArcadeGame directory

--- ArcadeGame/sheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="sheet" tilewidth="70" tileheight="70" spacing="2" tilecount="78" columns="13">
- <image source="../../Documents/StralenyaProject4/assets/Ice expansion/sheet.png" width="980" height="490"/>
-</tileset>
